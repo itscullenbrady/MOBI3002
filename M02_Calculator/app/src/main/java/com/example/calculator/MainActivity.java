5be15f26513ba3604694726e898221c407584654
Ize <cullenbrady6698@gmail.com>
2024-09-17 20:12:33 -0300

    files

---
+++ M02_Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText textN1, textN2, textANS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity", "App started");
+
+        textN1 = (EditText) findViewById(R.id.inputNumber1);
+        textN2 = (EditText) findViewById(R.id.inputNumber2);
+        textANS = (EditText) findViewById(R.id.editTextNumAns);
+
+        // Add button
+        Button buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonAdd.setOnClickListener(v -> performOperation((d1, d2) -> d1 + d2, "+"));
+
+        // Subtract button
+        Button buttonSubtract = (Button) findViewById(R.id.buttonSubtract);
+        buttonSubtract.setOnClickListener(v -> performOperation((d1, d2) -> d1 - d2, "-"));
+
+        // Multiply button
+        Button buttonMultiply = (Button) findViewById(R.id.buttonMultiply);
+        buttonMultiply.setOnClickListener(v -> performOperation((d1, d2) -> d1 * d2, "*"));
+
+        // Divide button
+        Button buttonDivide = (Button) findViewById(R.id.buttonDivide);
+        buttonDivide.setOnClickListener(v -> performOperation((d1, d2) -> {
+            if (d2 != 0) {
+                return d1 / d2;
+            } else {
+                return Double.NaN; // Or display an error message
+            }
+        }, "/"));
+    }
+
+    private void performOperation(Operation operation, String operatorSymbol) {
+        Double d1 = 0.0;
+        Double d2 = 0.0;
+        Double answer = 0.0;
+
+        try {
+            d1 = Double.parseDouble(textN1.getText().toString());
+            d2 = Double.parseDouble(textN2.getText().toString());
+            answer = operation.calculate(d1, d2);
+        } catch (Exception e) {
+            Log.w("M01_Calculator", "Operation Selected with no inputs ... " + answer);
+        }
+
+        textANS.setText(answer.toString());
+        Log.w("M01_Calculator", "Operation Selected with => " + d1 + " " + operatorSymbol + " " + d2 + "=" + answer);
+    }
+
+    interface Operation {
+        double calculate(double d1, double d2);
+    }
+}
